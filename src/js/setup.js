05870a52ed80ec814d71770f9b902d85b87738c2
helloItIsThomas <helloitisthomasm@gmail.com>
2025-02-05 13:19:26 -0500

    initial cursor port seems to be working

--- src/js/setup.js
+++ src/js/setup.js
@@ -1,3 +1,26 @@
+import { sv } from "./cursor/variables.js";
+// import { cursorSetup } from "./cursor/cursorSketch.js";
+import {
+  Application,
+  Assets,
+  Buffer,
+  BufferUsage,
+  Container,
+  Geometry,
+  Mesh,
+  Point,
+  Shader,
+  Sprite,
+  Texture,
+} from "/js/pixi/pixi.min.mjs";
+
+// import { BloomFilter } from "pixi-filters/bloom";
+// import { GlowFilter } from "pixi-filters/glow";
+
+import { loadShaders } from "./cursor/loadShaders.js";
+import { render } from "./cursor/render.js";
+import { Triangle } from "./cursor/triangle.js";
+
 document.addEventListener("DOMContentLoaded", async () => {
   console.log("running general setup");
 
@@ -57,4 +80,138 @@ document.addEventListener("DOMContentLoaded", async () => {
   // SETUP PARALLAX FISH end
 
   gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+
+  mySetup();
 });
+
+async function mySetup() {
+  sv.pApp = new Application();
+
+  await sv.pApp.init({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    // backgroundColor: 0x071134,
+    backgroundColor: 0x000,
+    // transparent: true,
+    // resolution: 3,
+    resizeTo: window,
+  });
+
+  const cursorCanvas = document.getElementById("cursorCanvas");
+  // const pixiCanvas = document.createElement("canvas");
+  cursorCanvas.appendChild(sv.pApp.canvas);
+
+  // document.body.appendChild(sv.pApp.canvas);
+
+  const spinnyBG = await Assets.load("../assets/images/dot2.png");
+
+  const { vertex, fragment } = await loadShaders();
+
+  const instancePositionBuffer = new Buffer({
+    data: new Float32Array(sv.totalTriangles * 2),
+    usage: BufferUsage.VERTEX | BufferUsage.COPY_DST,
+  });
+
+  const alphaBuffer = new Buffer({
+    data: new Float32Array(sv.totalTriangles),
+    usage: BufferUsage.VERTEX | BufferUsage.COPY_DST,
+  });
+
+  sv.triangles = [];
+
+  for (let i = 0; i < sv.totalTriangles; i++) {
+    sv.triangles[i] = new Triangle(
+      i,
+      Math.random() * sv.pApp.screen.width,
+      Math.random() * sv.pApp.screen.height,
+      Math.random() * 0.05
+    );
+  }
+
+  const cellW = 10;
+  const cellH = cellW;
+  const geometry = new Geometry({
+    topology: "triangle-strip",
+    instanceCount: sv.totalTriangles,
+    attributes: {
+      aPosition: [
+        0.0,
+        0.0,
+        cellW,
+        0.0,
+        cellW,
+        cellH,
+        cellW,
+        cellH,
+        0.0,
+        cellH,
+        0.0,
+        0.0,
+      ],
+      aUV: [0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0],
+      aPositionOffset: {
+        buffer: instancePositionBuffer,
+        instance: true,
+      },
+      aAlpha: {
+        buffer: alphaBuffer,
+        instance: true,
+      },
+      aSize: {
+        buffer: new Buffer({
+          data: new Float32Array(sv.triangles.map((t) => t.size)),
+          usage: BufferUsage.VERTEX | BufferUsage.COPY_DST,
+        }),
+        instance: true,
+      },
+    },
+  });
+
+  const gl = { vertex, fragment };
+
+  const shader = Shader.from({
+    gl,
+    resources: {
+      myTexture: spinnyBG.source,
+      uSampler: spinnyBG.source.style,
+      waveUniforms: {
+        mouseVelocity: { value: 0.5, type: "f32" },
+        mousePos: { value: sv.mousePos, type: "vec2<f32>" },
+        time: { value: sv.pApp.ticker.lastTime, type: "f32" },
+      },
+    },
+  });
+
+  const triangleMesh = new Mesh({
+    geometry,
+    shader,
+  });
+
+  const container = new Container();
+  container.width = sv.pApp.screen.width;
+  container.height = sv.pApp.screen.height;
+  container.filterArea = sv.pApp.screen;
+
+  // try glow, displacement, convolution?, shockwave?,
+  // container.filters = [
+  //   new BloomFilter({
+  //     strength: 10,
+  //     quality: 5,
+  //     kernelSize: 7,
+  // }),
+  // new GlowFilter({
+  // color: 0x64f0f5,
+  // distance: 1,
+  // innerStrength: 0,
+  // outerStrength: 5.0,
+  // }),
+  // ];
+
+  container.addChild(triangleMesh);
+  sv.pApp.stage.addChild(container);
+
+  sv.pApp.ticker.add(() => {
+    sv.clock = sv.pApp.ticker.lastTime * 0.001;
+    render(instancePositionBuffer, alphaBuffer, triangleMesh);
+  });
+}
