8056c62aeed2783085a0934b09dd68f731263d3c
helloItIsThomas <helloitisthomasm@gmail.com>
2025-02-05 00:18:53 -0500

    ~

--- public/js/setup.js
+++ public/js/setup.js
@@ -1,6 +1,32 @@
 document.addEventListener("DOMContentLoaded", async () => {
   console.log("running general setup");
 
+  const pageLoadingScreen = document.getElementById("pageLoadingScreen");
+
+  pageLoadingScreen.style.display = "block";
+  // pageLoadingScreen.style.display = "none";
+
+  gsap.from(
+    ".h2_0, .h1_0, .h0_5, .h1_5, .bm0, .bm1, .bm2, .bm3, .bs0, .bs1, .bs2, .bs3, .bser0, .bser1, .bser2, .bser3, hr",
+    {
+      y: 15,
+      duration: 0.5,
+      ease: "power1.inOut",
+      stagger: 0.05,
+      delay: 1.9,
+    }
+  );
+
+  gsap.to(pageLoadingScreen, {
+    opacity: 0,
+    duration: 1,
+    ease: "power3.out",
+    delay: 2,
+    onComplete: () => {
+      pageLoadingScreen.style.display = "none";
+    },
+  });
+
   // SETUP LENIS start
   // const lenis = new Lenis({
   // autoRaf: true,
--- src/projects/mediaTwo/index.md
+++
@@ -1,7 +0,0 @@
----
-title: "Format Reference"
-description: "a reference for formatting the front matter"
-# layout: "layouts/conservationMedia.njk"
----
-
-hello from mediaTwo
