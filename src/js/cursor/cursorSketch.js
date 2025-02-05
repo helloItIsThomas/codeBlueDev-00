c28f899c62971876484d32b83de6074da9d97cfb
helloItIsThomas <helloitisthomasm@gmail.com>
2025-02-05 12:41:35 -0500

    goint to implement matty's mouse

--- src/js/cursor/cursorSketch.js
+++ src/js/cursor/cursorSketch.js
@@ -1,20 +1,4 @@
-import {
-  Application,
-  Assets,
-  Buffer,
-  BufferUsage,
-  Container,
-  Geometry,
-  Mesh,
-  Shader,
-  Graphics,
-} from "/js/pixi/pixi.min.mjs";
-import { BlurFilter } from "pixi.js";
-import { BloomFilter } from "pixi-filters/bloom";
-import { ConvolutionFilter } from "pixi-filters/convolution";
-import { DisplacementFilter } from "pixi.js";
-import { GlowFilter } from "pixi-filters/glow";
-import { ShockwaveFilter } from "pixi-filters/shockwave";
+import { Application, Graphics } from "/js/pixi/pixi.min.mjs";
 
 const cursorCanvas = document.getElementById("cursorCanvas");
 const pixiCanvas = document.createElement("canvas");
@@ -25,13 +9,10 @@ await app.init({
   width: window.innerWidth,
   height: window.innerHeight,
   backgroundAlpha: 0,
-  resolution: 3,
   view: pixiCanvas,
   antialias: true,
   resizeTo: window,
 });
-
-const { vertex, fragment } = await loadShaders();
 
 const cursorGraphics = new Graphics();
 cursorGraphics.beginFill(0xffffff); // White color for the cursor
@@ -39,7 +20,8 @@ cursorGraphics.drawCircle(0, 0, 10); // Draw a circle with radius 10
 cursorGraphics.endFill();
 cursorGraphics.x = app.renderer.width / 2; // Center the cursor initially
 cursorGraphics.y = app.renderer.height / 2;
-
+// app.renderer.plugins.interaction.defaultCursorStyle = "inherit";
+// app.renderer.events.cursorStyles.default = "inherit";
 app.renderer.events.cursor = "none";
 app.renderer.events.setCursor("none");
 
