4535eade1b93b6f02cb69bc26c94351c0217d2d3
helloItIsThomas <helloitisthomasm@gmail.com>
2025-02-11 18:09:44 -0500

    time to add the text box expander thing

--- public/js/nav.js
+++ public/js/nav.js
@@ -4,19 +4,17 @@ export function styleThisPageNavLink(link) {
   link.style.opacity = 1;
 }
 
-document.getElementById("hamburgerMenu").addEventListener("click", () => {
-  toggleMobileMenu();
-});
-
-function toggleMobileMenu() {
-  const mobileMenu = document.getElementById("mobileMenu");
-  if (mobileMenu.style.display === "none") {
-    mobileMenu.style.display = "flex";
-    document.body.style.overflow = "hidden";
-    sv.lenis.stop();
-  } else {
-    mobileMenu.style.display = "none";
-    document.body.style.overflow = "auto";
-    sv.lenis.start();
-  }
+export function toggleMobileMenu() {
+  document.getElementById("hamburgerMenu").addEventListener("click", () => {
+    const mobileMenu = document.getElementById("mobileMenu");
+    if (mobileMenu.style.display === "none") {
+      mobileMenu.style.display = "flex";
+      document.body.style.overflow = "hidden";
+      sv.lenis.stop();
+    } else {
+      mobileMenu.style.display = "none";
+      document.body.style.overflow = "auto";
+      sv.lenis.start();
+    }
+  });
 }
