994446424ac31dae8eefba89e14756f372a4f3cd
helloItIsThomas <helloitisthomasm@gmail.com>
2025-02-12 13:11:18 -0500

    see todo5

--- src/js/nav.js
+++ src/js/nav.js
@@ -7,14 +7,28 @@ export function styleThisPageNavLink(link) {
 export function toggleMobileMenu() {
   document.getElementById("hamburgerMenu").addEventListener("click", () => {
     const mobileMenu = document.getElementById("mobileMenu");
+
     if (mobileMenu.style.display === "none") {
       mobileMenu.style.display = "flex";
       document.body.style.overflow = "hidden";
       sv.lenis.stop();
+      gsap.to(mobileMenu, {
+        duration: 0.5,
+        ease: "power3.out",
+        opacity: 1,
+        onComplete: () => {},
+      });
     } else {
-      mobileMenu.style.display = "none";
-      document.body.style.overflow = "auto";
-      sv.lenis.start();
+      gsap.to(mobileMenu, {
+        duration: 0.5,
+        ease: "power3.in",
+        opacity: 0,
+        onComplete: () => {
+          mobileMenu.style.display = "none";
+          document.body.style.overflow = "auto";
+          sv.lenis.start();
+        },
+      });
     }
   });
 }
