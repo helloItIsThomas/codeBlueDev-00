5853b63c603a9cb936e16418f8db8c46f8cba7de
helloItIsThomas <helloitisthomasm@gmail.com>
2025-02-14 03:36:31 -0500

    ~

--- public/js/smartTextBox.js
+++ public/js/smartTextBox.js
@@ -1,22 +1,3 @@
-// export function initializeExpandCollapse() {
-//   console.log("initializeExpandCollapse");
-
-//   const container = document.querySelector(".magicTextContainer");
-//   const button = document.querySelector(".caret-button");
-
-//   button.addEventListener("click", () => {
-//     container.classList.toggle("expanded");
-//     button.classList.toggle("expanded");
-
-//     const isExpanded = container.classList.contains("expanded");
-//     button.setAttribute("aria-expanded", isExpanded);
-//     button.setAttribute(
-//       "aria-label",
-//       isExpanded ? "Collapse text" : "Expand text"
-//     );
-//   });
-// }
-
 export function initializeExpandCollapse() {
   console.log("initializeExpandCollapse");
 
